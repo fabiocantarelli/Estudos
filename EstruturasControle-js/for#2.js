27ab77c8f73d8956956355b5ea9614960553955d
fabiocantarelli <fabiocantarelli@devswolf.com.br>
2020-03-26 11:54:39 -0300

    Update

---
+++ EstruturasControle-js/for#2.js
@@ -0,0 +1,15 @@
+const notas = [6.7, 8.1, 9.9, 4.2, 7.2, 7.1, 1.9]
+    // "in" utiliza para acessar o índice do array
+for (i in notas) {
+    console.log(i, notas[i])
+}
+
+/*
+0 6.7
+1 8.1
+2 9.9
+3 4.2
+4 7.2
+5 7.1
+6 1.9
+*/
